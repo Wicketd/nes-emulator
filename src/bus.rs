1e14f592deb68f49699023b842258309bb270297
Thierry Doornbos <t.doornbos95@gmail.com>
2020-08-07 14:51:48 +0200

    Start of partial rebuild

--- src/bus.rs
+++ src/bus.rs
@@ -1,78 +1,27 @@
-use crate::types::{Result, Address};
+use crate::types::Result;
 
 pub struct Bus {
-    bytes: [u8; Self::WIDTH],
+    // TODO: replace with devices
+    bytes: [u8; Self::LENGTH],
 }
 
 impl Bus {
-    const WIDTH: usize = 0x10000;
+    const LENGTH: usize = u16::MAX as usize + 1;
 
     pub fn new() -> Self {
-        Self { bytes: [0; Self::WIDTH] }
+        Self { bytes: [0; Self::LENGTH] }
     }
 
-    // TODO: error handling
-    pub fn read(&self, address: Address) -> u8 {
+    pub fn read(&self, address: u16) -> u8 {
         self.bytes[address as usize]
     }
 
-    pub fn read_zp(&self, address: u8) -> u8 {
-        self.read(address.into())
-    }
-
-    pub fn read_u16(&self, address: Address) -> Result<u16> {
-        if address as usize + 1 >= Self::WIDTH {
-            Err(anyhow!("reading 2 bytes from `${:04X}` would exceed max bus address `${:04X}`", address, Self::WIDTH - 1))
-        } else {
-            Ok(u16::from_le_bytes([self.read(address), self.read(address + 1)]))
-        }
-    }
-
-    pub fn read_zp_u16(&self, address: u8) -> Result<u16> {
+    pub fn read_u16(&self, address: u16) -> Result<u16> {
         if let Some(_) = address.checked_add(1) {
-            Ok(self.read_u16(address.into())?)
+            let bytes = [self.read(address), self.read(address + 1)];
+            Ok(u16::from_le_bytes(bytes))
         } else {
-            Err(anyhow!("writing 2 bytes to `${:04X}` would exceed the maximum zero-page address of `${:04X}`", address, u8::MAX))
-        }
-    }
-
-    pub fn read_n(&self, address: Address, n: u16) -> Vec<u8> {
-        let mut bytes = Vec::new();
-
-        for i in 0..n {
-            bytes.push(self.read(address + i));
-        }
-
-        bytes
-    }
-
-    pub fn write(&mut self, address: Address, value: u8) {
-        self.bytes[address as usize] = value;
-    }
-
-    pub fn write_zp(&mut self, address: u8, value: u8) {
-        self.write(address as Address, value);
-    }
-
-    pub fn write_u16(&mut self, address: Address, value: u16) -> Result {
-        if address as usize + 1 >= Self::WIDTH {
-            Err(anyhow!("writing 2 bytes to `${:04X}` would exceed the maximum bus address of `${:04X}`", address, Self::WIDTH - 1))
-        } else {
-            let bytes = u16::to_le_bytes(value);
-            self.write(address, bytes[0]);
-            self.write(address + 1, bytes[1]);
-            Ok(())
-        }
-    }
-
-    pub fn write_zp_u16(&mut self, address: u8, value: u16) -> Result {
-        if let Some(_) = address.checked_add(1) {
-            let bytes = u16::to_le_bytes(value);
-            self.write(address as Address, bytes[0]);
-            self.write(address as Address + 1, bytes[1]);
-            Ok(())
-        } else {
-            Err(anyhow!("writing 2 bytes to `${:04X}` would exceed the maximum zero-page address of `${:04X}`", address, u8::MAX))
+            Err(anyhow!("address out of bounds"))
         }
     }
 }
---
+++ src/cpu.rs
@@ -0,0 +1,79 @@
+use crate::bus::Bus;
+use crate::types::Result;
+
+const ADDRESS_NMI: u16 = 0xFFFA;
+const ADDRESS_RESET: u16 = 0xFFFC;
+const ADDRESS_IRQ: u16 = 0xFFFE;
+
+pub struct Cpu {
+    bus: Bus,
+    registers: RegisterSet,
+    vectors: VectorSet,
+}
+
+impl Cpu {
+    pub fn new(bus: Bus) -> Result<Self> {
+        let vectors = VectorSet {
+            nmi: bus.read_u16(ADDRESS_NMI)?,
+            reset: bus.read_u16(ADDRESS_RESET)?,
+            irq: bus.read_u16(ADDRESS_IRQ)?,
+        };
+
+        let mut registers = RegisterSet::new();
+        registers.pc = vectors.reset;
+
+        Ok(Self { bus, registers, vectors })
+    }
+
+    pub fn start(&mut self) -> Result {
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct RegisterSet {
+    a: u8,
+    x: u8,
+    y: u8,
+    s: u8,
+    p: StatusFlags,
+    pc: u16,
+}
+
+impl RegisterSet {
+    fn new() -> Self {
+        Self {
+            a: 0,
+            x: 0,
+            y: 0,
+            s: 0xFF,
+            p: StatusFlags::empty(),
+            pc: 0,
+        }
+    }
+}
+
+struct VectorSet {
+    nmi: u16,
+    reset: u16,
+    irq: u16,
+}
+
+bitflags! {
+    struct StatusFlags: u8 {
+        const NEGATIVE = 0b1000_0000;
+        const OVERFLOW = 0b0100_0000;
+        const BREAK_LEFT = 0b0010_0000;
+        const BREAK_RIGHT = 0b0001_0000;
+        const DECIMAL = 0b0000_1000;
+        const INTERRUPT_DISABLE = 0b0000_0100;
+        const ZERO = 0b0000_0010;
+        const CARRY = 0b0000_0001;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+}
