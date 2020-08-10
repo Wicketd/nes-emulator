cfaa9414956f1fe4a04c3049a4a06ef4fe32c65f
Thierry Doornbos <t.doornbos95@gmail.com>
2020-08-10 19:26:42 +0200

    Temporarily remove UI module

--- src/lib.rs
+++ src/lib.rs
@@ -9,23 +9,12 @@ mod types;
 mod bus;
 mod cpu;
 mod ppu;
-mod ui;
 
 use types::Result;
 use bus::Bus;
 use cpu::Cpu;
-use ui::RuntimeUi;
-use tui::backend::CrosstermBackend;
-use std::io;
 
 pub fn run() -> Result {
-    let mut _ui = {
-        let stdout = io::stdout();
-        let backend = CrosstermBackend::new(stdout);
-        RuntimeUi::new(backend)?
-    };
-    // ui.connect()?;
-
     let bus = Bus::new();
     let mut cpu = Cpu::new(bus)?;
     cpu.start()?;
